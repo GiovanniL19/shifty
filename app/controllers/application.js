1f466c501b1219044556b943aa15269fdc9869ae
Giovanni Lenguito <giovanni16.gl@gmail.com>
2016-08-28 22:54:52 +0100

    Implements night and day percentages for current month

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -18,6 +18,64 @@ export default Ember.Controller.extend({
     saveSettings: false,
   },
   loading: false,
+  percentageDaysThisMonth: 0,
+  percentageNightsThisMonth: 0,
+  
+  getDaysInMonth: function(month, year) {
+    var date = new Date(year, month, 1);
+    var days = [];
+  
+    while (date.getMonth() === month) {
+      days.push(moment(new Date(date)).format('DD/MM/YYYY'));
+      date.setDate(date.getDate() + 1);
+    }
+    
+    return days;
+    
+  },
+  
+  
+  generatePercentages: function(){
+    let controller = this;
+    var userID = this.get('user.id');
+    var totalDaysWorked = [];
+    
+    this.store.query('shift', {userID: userID, month: parseInt(moment(new Date()).format('M')), year: moment(new Date()).format('YYYY')}).then(function(shifts){      
+      shifts.forEach(function(shift){
+        if(shift.get('day')){
+          if(totalDaysWorked.indexOf(shift.get('dateText')) === -1){
+            totalDaysWorked.push(shift.get('dateText'));
+          }
+        }
+      });
+      
+      var days = controller.getDaysInMonth(parseInt(moment(new Date()).format('M')), moment(new Date()).format('YYYY'));
+      
+      var daysWorked = totalDaysWorked.length;
+      var totalDays = days.length;
+      var percentage = Math.floor((parseInt(daysWorked) / parseInt(totalDays)) * 100);
+
+      controller.set('percentageDaysThisMonth', percentage);
+      
+      totalDaysWorked = [];
+      
+      shifts.forEach(function(shift){
+        if(!shift.get('day')){
+          if(totalDaysWorked.indexOf(shift.get('dateText')) === -1){
+            totalDaysWorked.push(shift.get('dateText'));
+          }
+        }
+      });
+      
+      var daysWorked = totalDaysWorked.length;
+      var totalDays = days.length;
+      var percentage = Math.floor((parseInt(daysWorked) / parseInt(totalDays)) * 100);
+
+      controller.set('percentageNightsThisMonth', percentage);
+      
+      
+    });
+  }.observes('user.shifts.length'),
   
   isLoginPage: function(){
     let path = this.get('currentPath');
