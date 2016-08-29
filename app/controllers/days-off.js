3f6ead0a7bc026bd2757e180be1476a9ec7e67a5
Giovanni Lenguito <giovanni16.gl@gmail.com>
2016-08-29 13:16:17 +0100

    - Fixes days off

--- app/controllers/days-off.js
+++ app/controllers/days-off.js
@@ -5,32 +5,29 @@ export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
   
   months: [
-    {label: 'Januray', value: 1},
-    {label: 'Febuary', value: 2},
-    {label: 'March', value: 3},
-    {label: 'April', value: 4},
-    {label: 'May', value: 5},
-    {label: 'Jun', value: 6},
-    {label: 'July', value: 7},
-    {label: 'August', value: 8},
-    {label: 'September', value: 9},
-    {label: 'October', value: 10},
-    {label: 'November', value: 11},
-    {label: 'December', value: 12},
+    {label: 'Januray', value: 0},
+    {label: 'Febuary', value: 1},
+    {label: 'March', value: 2},
+    {label: 'April', value: 3},
+    {label: 'May', value: 4},
+    {label: 'June', value: 5},
+    {label: 'July', value: 6},
+    {label: 'August', value: 7},
+    {label: 'September', value: 8},
+    {label: 'October', value: 9},
+    {label: 'November', value: 10},
+    {label: 'December', value: 11},
     
   ],
   holidays: [],
   getDaysInMonth: function(month, year) {
     var date = new Date(year, month, 1);
-    var days = [];
-  
-    while (date.getMonth() === month) {
-      days.push(moment(new Date(date)).format('DD/MM/YYYY'));
-      date.setDate(date.getDate() + 1);
-    }
-    
-    return days;
-    
+     var days = [];
+     while (date.getMonth() === month) {
+        days.push(moment(new Date(date)).format('DD/MM/YYYY'));
+        date.setDate(date.getDate() + 1);
+     }
+     return days;
   },
 
   calculateDaysOff: function(month){
@@ -38,35 +35,37 @@ export default Ember.Controller.extend({
     
     let year = new Date().getFullYear();
     var user = this.get('application.user');
-    let chosenMonth = month;
-    if(chosenMonth == undefined){
-      chosenMonth = 1;
-    }
     
-    console.log('Month ' + parseInt(chosenMonth));
+    
+    var chosenMonth = 0;
+    
+    if(month !== undefined){
+      chosenMonth = parseInt(month); 
+    }    
+    
     var days = this.getDaysInMonth(parseInt(chosenMonth), year);
     
+    chosenMonth += 1;
     this.store.query('shift', {userID: user.get('id'), month: parseInt(chosenMonth), year: year}).then(function(shifts){
       if(shifts.get('length') === 0){
         controller.set('holidays', days);
       }else{
-        async.each(shifts, function(shift, nextShift){
+        shifts.forEach(function(shift){
           var date = shift.get('dateTimeStamp');
           
           async.eachSeries(days, function(day, nextDay){
             var i = days.indexOf(day);
-            if (moment(days[i]).format('DD/MM/YYYY') === moment.unix(date).format('DD/MM/YYYY')) {
-              console.log('Splicing');
+            if (days[i] === moment.unix(date).format('DD/MM/YYYY')) {
               days.splice(i, 1);
+              console.log('splice');
             }
             nextDay();
           }), function done(){
             nextShift();
           };
-        }, function done(){
-          controller.set('holidays', days);
-          console.log(days);
         });
+        controller.set('holidays', days);
+        console.log(days);
       }
     });
     
